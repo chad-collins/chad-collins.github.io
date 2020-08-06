686898232295009f9c5800410b081c9b4a6dd95b
Chad Collins <chadcollins.co@gmail.com>
2020-08-06 12:40:07 -0400

    added netlify form handling

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -2,17 +2,38 @@
   <Layout headerTitle="Contact Me" headerSubtitle="Let's get in touch">
     <section class="section">
       <div class="container">
-        <form class="form" method="POST" action="/success" data-netlify="true">
-          <label for="fname">Your Name</label>
-          <input type="text" id="name" name="name" placeholder="Your name.." />
+        <form
+          name="contact"
+          method="post"
+          v-on:submit.prevent="handleSubmit"
+          action="/success/"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+        >
+          <input type="hidden" name="form-name" value="contact" />
+          <p hidden>
+            <label>
+              Don’t fill this out:
+              <input name="bot-field" />
+            </label>
+          </p>
+          <div class="sender-info">
+            <div>
+              <label for="name" class="label">Your name</label>
+              <input type="text" name="name" v-model="formData.name" />
+            </div>
+            <div>
+              <label for="email">Your email</label>
+              <input type="email" name="email" v-model="formData.email" />
+            </div>
+          </div>
 
-          <label for="lname">Your Email</label>
-          <input type="email" id="email" name="email" placeholder="Your email.." />
+          <div class="message-wrapper">
+            <label for="message">Message</label>
+            <textarea name="message" v-model="formData.message"></textarea>
+          </div>
 
-          <label for="subject">Subject</label>
-          <textarea id="subject" name="subject" placeholder="Write something.."></textarea>
-
-          <button class="submit" type="submit">Submit</button>
+          <button type="submit">Submit form</button>
         </form>
       </div>
     </section>
@@ -20,7 +41,34 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      formData: {},
+    };
+  },
+  methods: {
+    encode(data) {
+      return Object.keys(data)
+        .map(
+          (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+        )
+        .join("&");
+    },
+    handleSubmit(e) {
+      fetch("/", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: this.encode({
+          "form-name": e.target.getAttribute("name"),
+          ...this.formData,
+        }),
+      })
+        .then(() => this.$router.push("/success"))
+        .catch((error) => alert(error));
+    },
+  },
+};
 </script>
 
 <style lang="scss">
