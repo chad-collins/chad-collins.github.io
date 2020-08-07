b12d8a4bb9fee91a100e9ae388613fab1b966c7b
Chad Collins <chadcollins.co@gmail.com>
2020-08-07 09:46:07 -0400

    added link to projects

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -1,5 +1,15 @@
 <template>
-  <Layout headerTitle="Uh Oh...." headerSubtitle="This isn't where I parked my car..."></Layout>
+  <Layout headerTitle="Uh Oh...." headerSubtitle="This isn't where I parked my car...">
+    <section class="section">
+      <div class="container">
+        <h2>Looking for a project?</h2>
+        <p>
+          Some of my projects links were updated in a recent redesign. See them all
+          <g-link to="/projects/">here.</g-link>
+        </p>
+      </div>
+    </section>
+  </Layout>
 </template>
 
 <script>
