683c2bb6248afa918f229d44b953d065399ce153
Chad Collins <chadcollins.co@gmail.com>
2020-10-02 00:38:04 -0400

    installed gh-pages

--- gridsome.config.js
+++ gridsome.config.js
@@ -4,40 +4,41 @@
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
-const path = require('path');
+const path = require("path");
 
 function addStyleResource(rule) {
   rule
-    .use('style-resource')
-    .loader('style-resources-loader')
+    .use("style-resource")
+    .loader("style-resources-loader")
     .options({
-      patterns: [path.resolve(__dirname, './src/assets/scss/globals.scss')],
+      patterns: [path.resolve(__dirname, "./src/assets/scss/globals.scss")],
     });
 }
 
 module.exports = {
-  siteName: 'Chad Collins Developer Portfolio',
-  siteDescription: 'A portfolio website for full stack developer Chad Collins',
+  siteName: "Chad Collins Developer Portfolio",
+  siteDescription: "A portfolio website for full stack developer Chad Collins",
+  siteUrl: "https://chad-collins.github.io",
   templates: {
-    Post: '/blog/:title',
-    Project: '/projects/:title',
+    Post: "/blog/:title",
+    Project: "/projects/:title",
   },
   plugins: [
     {
-      use: '@gridsome/source-filesystem',
+      use: "@gridsome/source-filesystem",
       options: {
-        path: 'src/content/posts/**/*.md',
-        typeName: 'Post',
+        path: "src/content/posts/**/*.md",
+        typeName: "Post",
       },
     },
     {
-      use: '@gridsome/source-filesystem',
+      use: "@gridsome/source-filesystem",
       options: {
-        path: 'src/content/projects/**/*.md',
-        typeName: 'Project',
+        path: "src/content/projects/**/*.md",
+        typeName: "Project",
         refs: {
           tools: {
-            typeName: 'Tools',
+            typeName: "Tools",
           },
         },
       },
@@ -46,16 +47,16 @@ module.exports = {
   transformers: {
     //Add markdown support to all file-system sources
     remark: {
-      externalLinksTarget: '_blank',
-      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
-      plugins: ['@gridsome/remark-prismjs'],
+      externalLinksTarget: "_blank",
+      externalLinksRel: ["nofollow", "noopener", "noreferrer"],
+      plugins: ["@gridsome/remark-prismjs"],
     },
   },
   chainWebpack: (config) => {
-    config.resolve.alias.set('@images', '@/assets/images');
-    const types = ['vue-modules', 'vue', 'normal-modules', 'normal'];
+    config.resolve.alias.set("@images", "@/assets/images");
+    const types = ["vue-modules", "vue", "normal-modules", "normal"];
     types.forEach((type) =>
-      addStyleResource(config.module.rule('scss').oneOf(type))
+      addStyleResource(config.module.rule("scss").oneOf(type))
     );
   },
 };
